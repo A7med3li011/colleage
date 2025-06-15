31a7ff8bf2934a75ec408951f4bb5c90512e81d3
Ahmed Ali <a7med3li0111@gmail.com>
2025-06-16 00:58:07 +0300

    handle login

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
- import logo from "../assits/logo.jpg"
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assits/logo.jpg";
 export default function NavBar() {
   return (
-    <div className='flex justify-between items-center  px-3 py-2'>
-      <div><img className='w-[50px] h-[50px] rounded-full' src={logo} alt='logo'/> </div>
-      <ul className='flex '>
-      <li className='py-1 px-3'><Link to={"/home"}></Link>Home</li>
-      {/* <li className='py-1 px-3'><Link to={"/register"}>Register</Link></li> */}
-        <li className='py-1 px-3'><Link to={"/login"}>Login</Link></li>
+    <div className="flex justify-between items-center  px-3 py-2">
+      <div>
+        <img className="w-[50px] h-[50px] rounded-full" src={logo} alt="logo" />{" "}
+      </div>
+      <ul className="flex ">
+        {/* <li className='py-1 px-3'><Link to={"/register"}>Register</Link></li> */}
+        {!localStorage.getItem("TeacherToken") &&
+        !localStorage.getItem("token") ? (
+          <li className="py-1 px-3">
+            <Link to="/login">Login</Link>
+          </li>
+        ) : (
+          <li
+            className="py-1 px-3 cursor-pointer"
+            onClick={() => {
+              localStorage.clear();
+
+              window.location.href = "/login"; // or use navigate from react-router
+            }}
+          >
+            Logout
+          </li>
+        )}
       </ul>
     </div>
-  )
+  );
 }
